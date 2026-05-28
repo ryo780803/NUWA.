2a30d705cd7e7278efcc661de09331757dffa3fc
大塚崚平 <ryo@ootsukanoMacBook-Air.local>
2026-05-28 19:43:58 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://github.com/ryo780803/NUWA..git',
+  base: '/nuwa-site/',
+  integrations: [tailwind()]
 });
