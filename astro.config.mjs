dd0ee8ce8b7551486727f66c26bf019f8b8165b1
大塚崚平 <ryo@ootsukanoMacBook-Air.local>
2026-05-28 20:06:43 +0900

    remove tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://ryo780803.github.io',
-  base: '/NUWA/',
-  integrations: [tailwind()]
+  base: '/NUWA./',
 });
