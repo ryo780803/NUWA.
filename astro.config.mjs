ff3967f2f69c307b379ae2a2e3cec7cfcf57c6cd
大塚崚平 <ryo@ootsukanoMacBook-Air.local>
2026-05-28 20:20:41 +0900

    fix tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://ryo780803.github.io',
   base: '/NUWA./',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
