5df1e9a831f0b4cf748786b1395858fe210fecea
大塚崚平 <ryo@ootsukanoMacBook-Air.local>
2026-05-28 19:49:28 +0900

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://github.com/ryo780803/NUWA..git',
-  base: '/nuwa-site/',
+  site: 'https://ryo780803.github.io',
+  base: '/NUWA/',
   integrations: [tailwind()]
 });
